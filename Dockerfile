e8b51c24aae725f5f2b561d0758209f4b053e00d
Dennis Miasoutov <dennis.miasoutov@mocoding.com>
2019-01-02 17:57:56 -0500

    Migrated to .net core 2.2

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mocoding/dotnet-node:2.1.6-10.x
+FROM mocoding/dotnet-node:2.2-10.x
 
 # Copy the app
 COPY . /app
--- src/server/Program.cs
+++ src/server/Program.cs
@@ -19,6 +19,7 @@ namespace spa_template
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
